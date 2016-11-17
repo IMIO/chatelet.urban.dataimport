e6dff262ab39ced1a93121433b2c8ea96f5acc38
jjaumotte <julien.jaumotte@imio.be>
2016-11-17 11:08:37 +0100

    Add mapping key for EnvClass (1&3) : state, decision date, decision, event date Add folderZones (to complete)

--- src/chatelet/urban/dataimport/acropole/valuesmapping.py
+++ src/chatelet/urban/dataimport/acropole/valuesmapping.py
@@ -5,49 +5,17 @@ from imio.urban.dataimport.mapping import table
 VALUES_MAPS = {
     'type_map': table({
         'header': ['portal_type', 'foldercategory'],
-        -88291: ['', ''],  # Abattage d'arbre, ne pas reprendre ces dossiers
-        -67348: ['', ''],  # ne pas reprendre ces dossiers (DIVENV)
+        -67348: ['EnvClassOne', ''],  # Permis d’environnement classe 1
         -62737: ['ParcelOutLicence', 'lap'],
-        -57728: ['', ''],  # ne pas reprendre ces dossiers
-        -53925: ['', ''],  # Permis unique, ne pas reprendre ces dossiers
-        -52990: ['', ''],  # Article 65, ne pas reprendre ces dossiers
+        -53925: ['', ''],  # Permis unique, ne pas reprendre pour l'instant
         -49306: ['Article127', ''],
-        -46623: ['', ''],  # permis d'environnement classe 3
+        -46623: ['EnvClassThree', ''],  # permis d'environnement classe 3
         -42575: ['BuildLicence', 'uap'],
-        -40086: ['ParcelOutLicence', 'lap'],
-        -37624: ['', ''],  # permis d'environnement classe 1
-        -36624: ['', 'infraction'],  # infractions, ne pas reprendre
-        -34766: ['NotaryLetter', ''],  # lettre de notaire
-        -28278: ['', ''],  # permis d'environnement classe 1
-        -26124: ['ParcelOutLicence', 'lap'],
-        -25638: ['MiscDemand', 'dpr'],
-        -21454: ['', 'reclam'],  # reclamations, ne pas reprendre
-        -20646: ['Article127', ''],
-        -19184: ['', ''],  # permis d'environnement classe 2
-        -17277: ['BuildLicence', 'uap'],
+        -37624: ['EnvClassOne', ''],  # permis d'environnement classe 1
+        -36624: ['', 'infraction'],  # infractions, implémentation prévue dans le futur
+        -34766: ['NotaryLetter', ''],  # lettre de notaire (art 85)
         -15200: ['Declaration', ''],
-        -14333: ['', 'reclam'],  # reclamations, ne pas reprendre
         -14179: ['Division', ''],
-        -13467: ['', 'infraction'],  # infractions, ne pas reprendre
-        -11889: ['BuildLicence', 'uap'],
-        -10362: ['MiscDemand', 'dpr'],  # demande de principe
-        -7812: ['Article127', ''],
-        -5976: ['', ''],  # permis d'environnement classe 3
-        -5753: ['NotaryLetter', ''],  # lettre de notaire
-        -3575: ['', ''],  # permis d'environnement classe 2
-        -2982: ['UrbanCertificateOne', ''],  # A faire !!!
-        -1972: ['ParcelOutLicence', 'lap'],
-        0: ['', ''],  # ne pas reprendre ces dossiers
-        537481: ['BuildLicence', 'uap'],
-        585827: ['ParcelOutLicence', 'lap'],
-        596954: ['UrbanCertificateOne', ''],  # A faire !!!
-        598613: ['', ''],  # Permis unique classe 1, ne pas reprendre ces dossiers
-        598861: ['', ''],  # Permis unqiue classe 2, ne pas reprendre ces dossiers
-        599084: ['', ''],  # Classe 3, as reprendre ces dossiers
-        600326: ['Division', ''],
-        917233: ['BuildLicence', 'uap'],  # TODO id présent dans Châtelet (RA-B): type permis d'urbanisme : à valider
-        1057715: ['ParcelOutLicence', 'lap'],  # TODO id présent dans Châtelet (RA-L): type permis d'urbanisation : à valider
-        3937207: ['', ''],  # Abattage d'arbre, ne pas reprendre ces dossiers
     }),
 
     # pour la reférence, virer le 'RA' ou 'RG'
@@ -57,15 +25,22 @@ VALUES_MAPS = {
 
     # octroi/refus
     'state_map': {
-        -46L: 'refuse',  # -46 = annulé par le FD
+        -58L: '',  # chatelet => TODO validate
         -49L: 'accept',  # -49 = octroyé
+        -46L: 'refuse',  # -46 = annulé par le FD
+        -30L: '',  # chatelet => TODO validate
+        -27L: '',  # chatelet => TODO validate
         -26L: 'accept',  # -26 = octroyé
         -20L: '',  # chatelet => TODO validate
         -19L: 'retire',  # -19 = périmé
+        -17L: '',  # chatelet => TODO validate
+        -15L: '',  # chatelet => TODO validate
         -14L: 'accept',  # -14 = octroyé
         -12L: 'accept',  # -12 = octroyé (validé par Fl)
         -11L: 'retire',  # -11 = retiré
         -10L: 'retire',  # -10 = retiré (validé par Fl)
+        -8L: '',   # chatelet => TODO validate
+        -7L: '',   # chatelet => TODO validate
         -6L: 'accept',  # -6 = octroyé (validé par Fl)
         -5L: 'refuse',  # -5 = refusé
         -4L: 'retire',  # -4 = suspendu
@@ -104,6 +79,8 @@ VALUES_MAPS = {
         'UrbanCertificateTwo': ['octroi-cu2', '', 'depot-de-la-demande', '', '', ''],
         'NotaryLetter': ['octroi-lettre-notaire', '', 'depot-de-la-demande', '', '', ''],
         'Article127': ['delivrance-du-permis-octroi-ou-refus', '', 'depot-de-la-demande', '', '', ''],
+        'EnvClassOne': ['decision', '', 'depot-de-la-demande', '', '', ''],
+        'EnvClassThree': ['acceptation-de-la-demande', '', 'depot-de-la-demande', '', '', ''],
     }),
 
     'titre_map': {
@@ -122,13 +99,15 @@ VALUES_MAPS = {
     'event_deposit_name_map': {
         'BuildLicence': [u'récépissé'],
         'ParcelOutLicence': [u'récépissé'],
-        'Article127': [u'Début du dossier', u'dépôt dossier', u'réception de la demande du FD', u'dossier complet'],  # dossier complet ask by Florennes
+        'Article127': [u'Début du dossier', u'dépôt dossier', u'réception de la demande du FD', u'dossier complet'],  # dossier complet asked by Florennes
         'NotaryLetter': [u'Réception demande'],
         'UrbanCertificateOne': [u'réception demande'],
         'UrbanCertificateTwo': [u'réception demande'],
         'Declaration': [u'dépôt ou envoi de la déclaration'],
         'Division': [u'récépissé'],
         'MiscDemand': [u'Accusé de réception', u'réception demande'],
+        'EnvClassOne': [u'accusé de réception', u'réception demande', u'récépissé'],
+        'EnvClassThree': [u'Accusé de réception', u'réception demande', u'récépissé'],
     },
 
     'event_decision_date_map': {
@@ -141,6 +120,8 @@ VALUES_MAPS = {
         'Declaration': [u'délivrance permis'],
         'Division': [u'délivrance permis'],
         'MiscDemand': [u'Délivrance autorisation'],
+        'EnvClassOne': [u'délivrance permis'],
+        'EnvClassThree': [u'date passage au Collège'],
     },
 
     'event_decision_map': {
@@ -153,6 +134,8 @@ VALUES_MAPS = {
         'Declaration': [u'délivrance permis'],
         'Division': [u'délivrance permis'],
         'MiscDemand': [u'Délivrance autorisation'],
+        'EnvClassOne': [u'octroi du permis'],
+        'EnvClassThree': [u'date de décision exécutoire'],
     },
 
     'event_college_report_date_map': {
@@ -189,12 +172,39 @@ VALUES_MAPS = {
         u"dgrne": "dgrne",
         u"belgacom": "belgacom",
         u"autres": "autres",
+
+        # u"Administration des mines" : "",
+        # u"Belgacom" : "",
+        # u"Brutele" : "",
+        # u"CCATM" : "",
+        # u"Cellule Ram" : "",
+        # u"Commissaire voyer" : "",
+        # u"Conseiller en mobilité" : "",
+        # u"Conseiller en énergie" : "",
+        # u"DGO01" : "",
+        # u"DGRNE division de l'eau" : "",
+        # u"DGRNE office wallon des déchets" : "",
+        # u"Direction des routes de Charleroi" : "",
+        # u"division de la police d'environnement" : "",
+        # u"DNF" : "",
+        # u"Logis châtelettain" : "",
+        # u"Ministère de l'Agriculture" : "",
+        # u"ORES" : "",
+        # u"Service d'intervention incendie" : "",
+        # u"service environnement" : "",
+        # u"Service travaux" : "",
+        # u"SNCB" : "",
+        # u"SPW service patrimoine" : "",
+        # u"SWDE" : "",
+        # u"TEC" : "",
+        # u"zone de police" : "",
     },
 
     'zoneDictionary': {
+        # déjà présentes
         u"zone d'habitat": "zh",
-        u"zone d'habitat à caractère rural": "zhcr",
-        u"zone d'habitat à caractère rural sur +/- 50 m et le surplus en zone agricole": "zhcrza",
+        u"zone d'habitat à caractère rural": "zhrc",
+        u"zone d’habitat à caractère rural sur +/- 50 m et le surplus en zone agricole": "zhrcza",
         u"zone de services publics et d'équipements communautaires": "zspec",
         u"zone de centre d'enfouissement technique": "zcet",
         u"zone de loisirs": "zl",
@@ -202,76 +212,112 @@ VALUES_MAPS = {
         u"zones d'activité économique industrielle": "zaei",
         u"zones d'activité économique spécifique agro-économique": "zaesae",
         u"zones d'activité économique spécifique grande distribution": "zaesgd",
+        u"zone d'extraction": "ze",
         u"zone d'aménagement différé à caractère industriel": "zadci",
         u"zone agricole": "za",
         u"zone forestière": "zf",
-        u"zone d'espaces verts": "zev",
+        u"zone d'espaces verts": "zef",
         u"zone naturelle": "zn",
         u"zone de parc": "zp",
-        u"zone natura 2000": "znatura2000",
+        u"zone Natura 2000": "znatura2000",
+        # TODO nouvelles zones à encoder
+        u"Zone d'habitat": "zh",
+        u"fonction centrale (2)": "fc",
+        u"zone urbaine centrale": "zuc",
+        u"zone d'habitat de forte densité": "zhfd",
+        u"aire à fonction centrale (2)": "afc",
+        u"zone d'habitat de densité moyenne à faible": "zhdmf",
+        u"aire de bâtisses semi-agglomérées (4)": "absa",
+        u"centre ancien de Châtelet (1A)": "cadc",
+        u"Zone d'espaces verts": "zev",
+        u"aire d'espaces verts et forestière (8)": "zevf",
+        u"zone d'intérêt paysager": "zip",
+        u"aire de bâtisses agglomérées (3)": "aba",
+        u"Zone de services publics et d'équipements communautaires": "zspec",
+        u"aire de grandes bâtisses en milieu isolé non accessibles au public (6B)": "agbminap",
+        u"zone d'équipements communautaires et de services publics": "zecsp",
+        u"aire de bâtisses pavillonnaires (5)": "abp",
+        u"aire rurale de périphérie (7)": "ardp",
+        u"Zone agricole": "za",
+        u"zone rurale": "zr",
+        u"zone d'application automatique de l'article 186 du CWATUPE": "zaaa186",
+        u"aire de bâtisses semi-agglomérées avec un périmètre de bâtisses répétitives": "absapbr",
+        u"aire d'espaces verts et forestière avec un périmètre de protection particulière": "aevfppp",
+        u"zone d'espace vert": "zef",
+        u"zone de constructions résidentielles en ordre continu": "zdcroc",
+        u"zone de commerce de détail facultatif": "zdcddf",
+        u"zone de construction en ordre contigü (hauteur sous corniche 7m max)": "zdcoc",
+        u"aire de bâtisses agglomérées avec un périmètre de bâtisses répétitives": "adbaapdbr",
+        u"Zone d'activité économique industrielle": "zaei",
+        u"zone industrielle": "zi",
+        u"grandes bâtisses en milieu isolé non accessibles au public + périmètre protection particulière": "gbeminapppp",
+        u"zone de recul": "zdr",
+        u"zone d'activité économique de type mixte (artisanat et PME)": "zaetm",
+        u"zone de dépôt et de développement artisanal et de PME": "zdddapme",
+        u"aire de grandes bâtisses en milieu isolé accessibles au public (6A)": "adgbmiap",
+        u"Zone de loisirs": "zdl",
+        u"Zone d'aménagement communal concerté": "zacc",
+        u"zone de voirie publique": "zdvp",
+        u"zone de construction en ordre contigü (hauteur max 24 m)": "zdcoc",
+        u"zone de constructions résidentielles secondaires": "zdcrs",
+        u"zone de cours et jardins": "zdcj",
+        u"Zone forestière": "zf",
+        u"centre ancien de Bouffioulx (1B)": "cadbouf",
+        u"aire rurale de périphérie avec un périmètre de protection particulière": "arpappp",
+        u"Zone d'ext. d'habitat à caractère rural": "zehacr",
+        u"zone d'extension d'habitat": "zeh",
+        u"zone de constructions résidentielles": "zdcr",
+        u"Zone d'activité économique mixte": "zaem",
+        u"zone d'implantation d'entreprises de services, à proportion importante d'espaces verts, ceinturant d": "",
+        u"zone d'accès privé à la voirie": "zapv",
+        u"aire de bâtisses semi-agglomérées avec un périmètre de valorisation naturelle": "absapvn",
+        u"zone à rénover": "zar",
+        u"zone de réservation": "zdres",
+        u"zone de construction en ordre contigü (hauteur sous corniche entre 10m et 13m)": "zdcoc1013",
+        u"zone de construction en ordre contigü (hauteur sous corniche de 10m max)": "zdcoc10max",
+        u"Zone d'aménagement différé à caractère industriel": "zadci",
+        u"aire d'espaces verts et forestière avec périmètre de valorisation naturelle et protection paysagère": "aevfapvnp",
         u"zone d'assainissement collectif": "zac",
-        u"zone de construction d'habitation fermée": "zchf",
-        u"zone de cours et jardins": "zcj",
-        u"zone de recul": "zr",
-        u"zone forestière d'intérêt paysager": "zfip",
-        u"zone faiblement habitée": "zfh",
-        u"zone de construction en annexe": "zca",
-        u"zone de construction d'habitation semi-ouverte": "zcso",
-        u"zone de construction d'habitation ouverte": "zcho",
-        u"zone de bâtisses agricoles": "zba",
-        u"zone d'habitat dans un périmètre d'intérêt culturel, historique ou esthétique": "zhche",
-        u"zone d'habitat à caractère rural sur une profondeur de 50 mètres": "zhcr50",
-        u"zone d'habitat à caractère rural sur une profondeur de 40 mètres": "zhcr40",
-        u"zone d'extraction": "ze",
-        u"zone d'ext. d'habitat": "zeh",
-        u"zone d'équipements communautaires et de services publics": "zspec",
-        u"zone d'équipement communautaire": "zec",
-        u"zone d'assainissement autonome": "zaa",
-        u"zone d'aménagement communal concerté mise en oeuvre": "zaccmeo",
-        u"zone d'aménagement communal concerté": "zacc",
-        u"zone boisée": "zb",
-        u"zone artisanale": "zart",
-        u"zone agricole pour partie": "zapp",
-        u"zone agricole pour le surplus": "zapls",
-        u"zone agricole et zone forestière": "zaezf",
-        u"zone agricole dans un périmètre d'intérêt paysager pour le surplus": "zapippls",
-        u"zone agricole dans un périmètre d'intérêt paysager": "zapip",
-        u"voirie": "zv",
-        u"sans affectation": "sa",
-        u"pv de constat d'infraction": "pvci",
-        u"plan d'eau": "pe",
-        u"périmètre de réservation sur 75 m de profondeur à partir de l'axe de la voirie": "pr75padlv",
-        u"infraction relevée mais sans PV": "pr75irspvpadlv",
-        u"aire de faible densité": "afd",
-        u"aire de moyenne densité": "amd",
-        u"aire de forte densité": "afod",
-        u"en partie dans un périmètre de réservation": "eppdr",
-        u"déclaré inhabitable": "di",
-        u"dans un périmètre d''intérêt culturel, historique ou esthétique": "pche",
-        u"dans un périmètre de réservation": "dpdr",
-        u"dans un périmètre d'intérêt paysager": "dpip",
-        u"faible": "fai",
-        u"très faible": "tfai",
-        u"eau": "eau",
-        u"élevé": "eleve",
-        u"éloignée": "eloi",
-        u"dossier en cours": "dec",
-        u"dans un périmètre d'intérêt culturel, historique ou esthétique": "dupiche",
-        u"zone d'habitat sur 50 m de profondeur": "zhas50dp",
+        u"Zone d'activité économique spécifique - Agro-économique": "zaesae",
+        u"zone d’activité économique mixte": "zaem",
+        u"Identique au Plan de secteur": "ipds",
+        u"aire rurale de périphérie avec un périmètre de valorisation naturelle": "arppvn",
+        u"zone mixte habitat, commerce et service": "zmhces",
+        u"zone de constructions commerciales et de services secondaires": "zdccss",
+        u"zone d'isolement (ou de protection, tampon)": "ziso",
+        u"zone de parcage arborée": "zpa",
+        u"zone pévue pour une piscine": "zpp",
+        u"zone d'immeubles commerciaux suivant prescriptions spéciales": "zicsps",
+        u"zone d'aménagement différé": "zad",
+        u"zone de commerce de détail obligatoire": "zdcddo",
+        u"aire d'espaces verts et forestière avec un périmètre de valorisation naturelle": "aevfpvn",
+        u"aire de bâtisses semi-agglomérées avec un périmètre de grandes bâtisses en milieu intégré": "adbsaapgbmi",
+        u"aire de bâtisses agglomérées avec un périmètre de grandes bâtisses en milieu intégré": "adbaapdgbmi",
+        u"Périmètre de réservation": "pdr",
+        u"Zone de non aedificandi": "zdnaedi",
+        u"zone de constructions publique ou à destination publique": "zdcpodp",
+        u"Aire de grande batisse en milieu isolé accessible au public": "adgbmiaap",
+        u"Zone d'implantation d'immeuble commerciaux": "ziic",
+        u"périmètre de protection particulière": "ppp",
+        u"zone de constructions résidentielles en ordre ouvert": "zdcreoo",
+        u"Périmètre d'intérêt paysager": "pip",
+        u"Aire de grandes bâtisses en milieu intégré": "adgbmi",
+        u"Zone réservée à l'implantation de logements à caractère social": "zridlacs",
+        u"Zone naturelle d'intérêts scientifiques ou réserve naturelle": "znisorn",
+        u"zone d'annexes artisanales ou commerciales": "zaaoc",
+        u"zone de blocs de constructions (hauteur sous corniche 23m max)": "zdbc23max",
+        u"zone publique (trottoir)": "zpt",
+        u"zone de construction en hauteur": "zdceh",
+        u"zone de parking": "zdp",
+        u"zone d'activité économique": "zae",
+        u"zone de captage": "zdc",
+        u"zone commerciale avec bureaux ou appartements aux étages": "zcabaae",
+        u"zone de construction en annexe": "zdcea",
+        u"zone de parc privé (voirie autorisée)": "zdppva",
     },
 
-    'raw_pca_Dictionary': {
-        u"ppa1part": "pca1",
-        u"ppa1": "pca1",
-        u"ppa1b": "pca1",
-        u"ppa1tflo": "pca1",
-        u"ppa1tfla": "pca1",
-        u"ppa2": "pca2",
-        u"ppa2part": "pca2",
-        u"ppa2b": "pca2",
-        u"ppa3": "pca3",
-        u"ppa3part": "pca3",
-        u"ppa3mod": "pca3",
+    'raw_pca_List': {
+        u"pca1", u"pca2", u"pca3", u"pca4",
     },
 
 }
